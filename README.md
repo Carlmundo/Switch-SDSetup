55a486418f3b14025ac3d92e89dacf9a805f8060
Carlmundo <Carlmundo@users.noreply.github.com>
2019-03-29 12:45:30 +0000

    Download from SDSetup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # SwitchSDSetup
+Get AtlasNX Kosmos updates from:
+https://www.sdsetup.com/console?switch#atmosphere;atmos_musthave;atmos_bootlogo;kosmos_toolkit;atmos_sigpatch;hbmenu;edizon;checkpoint;tinfoil;goldleaf;lockpick;retroarch;retroarch_snes9x2010;retroarch_pcsxra;retroarch_gengxp;hekate;
---
+++ retroarch/shaders/crt/shaders/guest/avg-lum.glsl
@@ -0,0 +1,132 @@
+#version 130
+/*
+   Average Luminance Shader
+   
+   Copyright (C) 2018 guest(r) - guest.r@gmail.com
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; either version 2
+   of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+   
+   Thanks to HunterK for the mipmap hint. :D  
+*/
+
+// Parameter lines go here:
+#pragma parameter grade "Raster Bloom Grade" 0.65 0.10 1.0 0.05
+
+#if defined(VERTEX)
+
+#if __VERSION__ >= 130
+#define COMPAT_VARYING out
+#define COMPAT_ATTRIBUTE in
+#define COMPAT_TEXTURE texture
+#else
+#define COMPAT_VARYING varying 
+#define COMPAT_ATTRIBUTE attribute 
+#define COMPAT_TEXTURE texture2D
+#endif
+
+#ifdef GL_ES
+#define COMPAT_PRECISION mediump
+#else
+#define COMPAT_PRECISION
+#endif
+
+COMPAT_ATTRIBUTE vec4 VertexCoord;
+COMPAT_ATTRIBUTE vec4 COLOR;
+COMPAT_ATTRIBUTE vec4 TexCoord;
+COMPAT_VARYING vec4 COL0;
+COMPAT_VARYING vec4 TEX0;
+
+uniform mat4 MVPMatrix;
+uniform COMPAT_PRECISION int FrameDirection;
+uniform COMPAT_PRECISION int FrameCount;
+uniform COMPAT_PRECISION vec2 OutputSize;
+uniform COMPAT_PRECISION vec2 TextureSize;
+uniform COMPAT_PRECISION vec2 InputSize;
+
+void main()
+{
+    gl_Position = MVPMatrix * VertexCoord;
+    COL0 = COLOR;
+    TEX0.xy = TexCoord.xy;
+}
+
+#elif defined(FRAGMENT)
+
+#if __VERSION__ >= 130
+#define COMPAT_VARYING in
+#define COMPAT_TEXTURE texture
+out vec4 FragColor;
+#else
+#define COMPAT_VARYING varying
+#define FragColor gl_FragColor
+#define COMPAT_TEXTURE texture2D
+#endif
+
+#ifdef GL_ES
+#ifdef GL_FRAGMENT_PRECISION_HIGH
+precision highp float;
+#else
+precision mediump float;
+#endif
+#define COMPAT_PRECISION mediump
+#else
+#define COMPAT_PRECISION
+#endif
+
+uniform COMPAT_PRECISION int FrameDirection;
+uniform COMPAT_PRECISION int FrameCount;
+uniform COMPAT_PRECISION vec2 OutputSize;
+uniform COMPAT_PRECISION vec2 TextureSize;
+uniform COMPAT_PRECISION vec2 InputSize;
+uniform sampler2D Texture;
+COMPAT_VARYING vec4 TEX0;
+// in variables go here as COMPAT_VARYING whatever
+
+// compatibility #defines
+#define Source Texture
+#define vTexCoord TEX0.xy
+
+#define SourceSize vec4(TextureSize, 1.0 / TextureSize) //either TextureSize or InputSize
+#define outsize vec4(OutputSize, 1.0 / OutputSize)
+
+#ifdef PARAMETER_UNIFORM
+// All parameter floats need to have COMPAT_PRECISION in front of them
+uniform COMPAT_PRECISION float grade;
+#else
+	#define grade 0.65
+#endif 
+
+void main()
+{
+	float xtotal = floor(InputSize.x/32.0);
+	float ytotal = floor(InputSize.y/32.0);
+	
+	float ltotal = 0.0;
+	vec2 dx  = vec2(SourceSize.z, 0.0)*32.0;
+	vec2 dy  = vec2(0.0, SourceSize.w)*32.0;
+	
+	for (float i = 0.0; i <= xtotal; i++)
+	{
+		for (float j = 0.0; j <= ytotal; j++)
+			{
+				ltotal += length(textureLod(Source, i*dx + j*dy, 5.0).rgb);
+			}
+   }
+   
+   ltotal = inversesqrt(3.0)*ltotal / ((xtotal+1.0)*(ytotal+1.0));
+   
+   FragColor = vec4(pow(ltotal, grade));
+} 
+#endif
